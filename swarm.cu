06607841e3286d364b917ca28b4b5bdbd483fabf
Joshimar Cordova <jcmonroy9@gmail.com>
2014-12-16 17:33:20 -0500

    Rename generic variables' names in device src code.

--- swarm.cu
+++ swarm.cu
@@ -1,6 +1,5 @@
 #define GLM_FORCE_RADIANS
-// #define FLOCKSIZE 10
-// #define MIN_COLLISON_AVOIDANCE 10
+
 #include <cuda.h>
 #include <iostream>
 #include <stdio.h>
@@ -24,20 +23,18 @@ __device__ int GPU_globalindex(){
                 threadIdx.x;
 }
 
-__device__ void closest_neighbors(glm::vec3*& points, int& n_points, int global_index, int total_number, glm::vec3 *c){
+__device__ void closest_neighbors(glm::vec3*& points, int& n_points, int global_index, int total_number, glm::vec3 *positions){
   int j;
   n_points = 0;
   for(j=0;j<total_number;++j){
     if(j != global_index){
-      if(glm::distance(c[j], c[global_index]) < FLOCKSIZE){
-        points[n_points] = c[j];
+      if(glm::distance(positions[j], positions[global_index]) < FLOCKSIZE){
+        points[n_points] = positions[j];
         n_points++;
       }
     }
   }
 }
-
-// __global__ Functions
 
 __device__ glm::vec3 avoidance(glm::vec3* points, int n_points, glm::vec3* cur_boid){
     glm::vec3 new_vec(0,0,0);
@@ -46,96 +43,104 @@ __device__ glm::vec3 avoidance(glm::vec3* points, int n_points, glm::vec3* cur_b
     return new_vec;
 }
 
-__global__ void GPU_update_vector(glm::vec3 *dj, glm::vec3 *c, int n){
+// __global__ Functions
+
+__global__ void GPU_update_vector(glm::vec3 *updated_directions, glm::vec3 *positions, int num_fishes){
         int index = GPU_globalindex();
-        if(index < n)
+        if(index < num_fishes)
         {
                 int n_points = 0;
-                glm::vec3* points = new glm::vec3[n];
-                closest_neighbors(points, n_points, index, n, c);
+                glm::vec3* points = new glm::vec3[num_fishes];
+                closest_neighbors(points, n_points, index, num_fishes, positions);
 
-                dj[index] += avoidance(points, n_points, &c[index]);
+                updated_directions[index] += avoidance(points, n_points, &positions[index]);
                 delete[] points;
         }
 }
 
-__global__ void GPU_Update(glm::mat4 *modelMatrices, glm::vec3 *d, glm::vec3 *dj, glm::vec3 *c, glm::vec3 *raxis, float *w, int n, float cT) {
+__global__ void GPU_Update(glm::mat4 *modelMatrices, glm::vec3 *directions, glm::vec3 *updated_directions, glm::vec3 *positions, glm::vec3 *raxis, float *angles, int num_fishes, float cT) {
         int i = GPU_globalindex();
-        if(i < n)
+        if(i < num_fishes)
         {
 
                 float theta = 0.0;
                 glm::vec3 cr(0,0,0);
-                if(glm::length(d[i]-dj[i]) > epsilon)
+                if(glm::length(directions[i]-updated_directions[i]) > epsilon)
                 {
-                        theta = glm::acos(glm::dot(glm::normalize(d[i]),glm::normalize(dj[i])));
-                        cr = glm::normalize(glm::cross(d[i],dj[i]));
+                        theta = glm::acos(glm::dot(glm::normalize(directions[i]),glm::normalize(updated_directions[i])));
+                        cr = glm::normalize(glm::cross(directions[i],updated_directions[i]));
                 }
 
                 if(glm::length(raxis[i]) > epsilon)
                 {
-                        modelMatrices[i] = glm::rotate(modelMatrices[i], -w[i], raxis[i]);
+                        modelMatrices[i] = glm::rotate(modelMatrices[i], -angles[i], raxis[i]);
                 }
-                modelMatrices[i] = glm::translate(modelMatrices[i],dj[i]*0.0125f);               // Falta Delta, reemp. por 0.0125
+                modelMatrices[i] = glm::translate(modelMatrices[i],updated_directions[i]*0.0125f);               // Falta Delta, reemp. por 0.0125
                 if(glm::length(raxis[i]) > epsilon)
                 {
-                        modelMatrices[i] = glm::rotate(modelMatrices[i], w[i], raxis[i]);
+                        modelMatrices[i] = glm::rotate(modelMatrices[i], angles[i], raxis[i]);
                 }
 
 
                 if(glm::length(cr) > epsilon)
                 {
                         modelMatrices[i] = glm::rotate(modelMatrices[i], theta, cr);
-                        raxis[i] = glm::normalize(glm::cross(glm::vec3(0.0,0.0,0.25),dj[i]));
-                        w[i] = glm::acos(glm::dot(glm::normalize(dj[i]),glm::normalize(glm::vec3(0.0,0.0,0.25))));;
+                        raxis[i] = glm::normalize(glm::cross(glm::vec3(0.0,0.0,0.25),updated_directions[i]));
+                        angles[i] = glm::acos(glm::dot(glm::normalize(updated_directions[i]),glm::normalize(glm::vec3(0.0,0.0,0.25))));;
                 }
 
-                d[i] = dj[i];
-                c[i] += d[i]*0.0125f;
+                directions[i] = updated_directions[i];
+                positions[i] += directions[i]*0.0125f;
 
         }
 }
 
-void update(glm::mat4 *modelMatrices, glm::vec3 *d, glm::vec3 *dj, glm::vec3 *c, glm::vec3 *raxis, float *w, int n, float cT) {
-
+void update(glm::mat4 *modelMatrices, glm::vec3 *directions,
+            glm::vec3 *updated_directions, glm::vec3 *positions,
+            glm::vec3 *raxis, float *angles, int num_fishes, float cT) {
         glm::mat4 *d_modelMatrices;
-        glm::vec3 *d_d, *d_dj, *d_c, *d_raxis;
-        float *d_w;
+        glm::vec3 *new_directions, *new_updated_directions, *new_positions, *new_raxis;
+        float *new_angles;
 
-        size_t m4size = n * sizeof(glm::mat4);
-        size_t v3size = n * sizeof(glm::vec3);
-        size_t fsize = n * sizeof(float);
+        size_t m4size = num_fishes * sizeof(glm::mat4);
+        size_t v3size = num_fishes * sizeof(glm::vec3);
+        size_t fsize  = num_fishes * sizeof(float);
 
         cudaMalloc(&d_modelMatrices, m4size);
-        cudaMalloc(&d_d, v3size);
-        cudaMalloc(&d_dj, v3size);
-        cudaMalloc(&d_c, v3size);
-        cudaMalloc(&d_raxis, v3size);
-        cudaMalloc(&d_w, fsize);
+        cudaMalloc(&new_directions, v3size);
+        cudaMalloc(&new_updated_directions, v3size);
+        cudaMalloc(&new_positions, v3size);
+        cudaMalloc(&new_raxis, v3size);
+        cudaMalloc(&new_angles, fsize);
 
         cudaMemcpy(d_modelMatrices, modelMatrices, m4size, cudaMemcpyHostToDevice);
-        cudaMemcpy(d_d, d, v3size, cudaMemcpyHostToDevice);
-        cudaMemcpy(d_dj, dj, v3size, cudaMemcpyHostToDevice);
-        cudaMemcpy(d_c, c, v3size, cudaMemcpyHostToDevice);
-        cudaMemcpy(d_raxis, raxis, v3size, cudaMemcpyHostToDevice);
-        cudaMemcpy(d_w, w, fsize, cudaMemcpyHostToDevice);
+        cudaMemcpy(new_directions, directions, v3size, cudaMemcpyHostToDevice);
+        cudaMemcpy(new_updated_directions, updated_directions, v3size, cudaMemcpyHostToDevice);
+        cudaMemcpy(new_positions, positions, v3size, cudaMemcpyHostToDevice);
+        cudaMemcpy(new_raxis, raxis, v3size, cudaMemcpyHostToDevice);
+        cudaMemcpy(new_angles, angles, fsize, cudaMemcpyHostToDevice);
 
-        dim3 grid(n,1,1);           // Max 2147483647 , 65535, 65535 blocks
+        dim3 grid(num_fishes,1,1);  // Max 2147483647 , 65535, 65535 blocks
         dim3 block(1,1,1);          // Max 1024 threads per block
-        GPU_update_vector<<<grid,block>>> (d_dj, d_c,n);
-        GPU_Update<<<grid,block>>> (d_modelMatrices, d_d, d_dj, d_c, d_raxis, d_w, n, cT);
+        GPU_update_vector<<<grid,block>>> (
+                        new_updated_directions,
+                        new_positions, num_fishes);
+        GPU_Update<<<grid,block>>> (
+                        d_modelMatrices, new_directions,
+                        new_updated_directions, new_positions,
+                        new_raxis, new_angles, num_fishes, cT);
 
         cudaMemcpy(modelMatrices, d_modelMatrices, m4size, cudaMemcpyDeviceToHost);
-        cudaMemcpy(d, d_d, v3size, cudaMemcpyDeviceToHost);
-        cudaMemcpy(dj, d_dj, v3size, cudaMemcpyDeviceToHost);
-        cudaMemcpy(c, d_c, v3size, cudaMemcpyDeviceToHost);
-        cudaMemcpy(raxis, d_raxis, v3size, cudaMemcpyDeviceToHost);
-        cudaMemcpy(w, d_w, fsize, cudaMemcpyDeviceToHost);
+        cudaMemcpy(directions, new_directions, v3size, cudaMemcpyDeviceToHost);
+        cudaMemcpy(updated_directions, new_updated_directions, v3size, cudaMemcpyDeviceToHost);
+        cudaMemcpy(positions, new_positions, v3size, cudaMemcpyDeviceToHost);
+        cudaMemcpy(raxis, new_raxis, v3size, cudaMemcpyDeviceToHost);
+        cudaMemcpy(angles, new_angles, fsize, cudaMemcpyDeviceToHost);
 
         cudaFree(d_modelMatrices);
-        cudaFree(d_d);
-        cudaFree(d_dj);
-        cudaFree(d_c);
-        cudaFree(d_raxis);
-        cudaFree(d_w);
+        cudaFree(new_directions);
+        cudaFree(new_updated_directions);
+        cudaFree(new_positions);
+        cudaFree(new_raxis);
+        cudaFree(new_angles);
 }
